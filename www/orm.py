9b143f7551efba3781d7602e792f78d8ed4aeed4
zhangqin <zhangqin601252@pwrd.com>
2019-04-15 14:26:17 +0800

    day2

--- www/orm.py
+++ www/orm.py
@@ -135,4 +135,24 @@ class ModelMetaclass(type):
         attrs['__primarykey__']=primarykey #用来存放主键的属性
         attrs['__fields__']=fields #存放除了主键以外的属性名
         attrs['__select__']='select `%s`, %s from `%s`' %(primarykey, ','.join(escaped_fields), tablename)
-        attrs['__insert__']='insert into `%s` (%s, `%s`) values (%s)' % (tablename, ','.join(escaped_fields), primarykey, )
+        attrs['__insert__']='insert into `%s` (%s, `%s`) values (%s)' % (tablename, ','.join(escaped_fields), primarykey, create_args_string(len(escaped_fields)+1))
+        attrs['__update__']='update `s%` set %s where `%s`= ?' % (tablename, ','.join(lambda f: '`%s`=?' %(mapping.get(f).name or f)), primarykey)
+        attrs['__delete__']='delete from `%s` where `%s`=?' % (tablename, primarykey)
+        return type.__new__(cls, name, bases, attrs)
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute %s" %key)
+
+    def __setattr__(self, key, value):
+        self[key]=value
+
+    def __getVaule__(self, key):
+        return getattr(self, key, None)
+
+    
